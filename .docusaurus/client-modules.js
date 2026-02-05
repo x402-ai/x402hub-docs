4687f3518ea24f41f24dcd1f6327452960eea2a8
Cap'n Coconut <jgrebski@gmail.com>
2026-02-05 10:04:45 -0500

    Deploy website

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/jacek/Documents/Workbook/clawpay-docs/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/jacek/Documents/Workbook/clawpay-docs/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jacek/Documents/Workbook/clawpay-docs/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jacek/Documents/Workbook/clawpay-docs/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/jacek/Documents/Workbook/clawpay-docs/docs/src/css/custom.css"),
-];
